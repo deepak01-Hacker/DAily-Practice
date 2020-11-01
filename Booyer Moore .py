1628a8e08fcc97cc8ac674162089b300e1ef9f7c
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2020-11-01 13:29:32 +0530

    String Matching Algorithm

---
+++ Booyer Moore .py
@@ -0,0 +1,28 @@
+def BadChar(pat,m):
+    bad = [1]*256
+    for i in range(m):
+        bad[ord(pat[i])] = m-i-1
+    bad[ord(pat[-1])] = m
+    return bad
+
+def BooyerMoore(pat,text):
+    m = len(pat)
+    n = len(text)
+    badchar = BadChar(pat,m)
+    i = 0 
+    while(i <= n-m):
+        j = m-1
+        while(j >= 0 and text[i+j] == pat[j] ):
+            j -= 1
+        if j < 0:
+            print(i,i+m)
+            i += m
+        else:
+            i += badchar[ord(text[i+m])]
+
+
+if __name__ == "__main__":
+    text = "ABAABDABA"
+    pat = "ABA"
+    print(BooyerMoore(pat,text))
+    
