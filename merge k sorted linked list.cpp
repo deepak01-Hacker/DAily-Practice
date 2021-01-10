2abeb72474b7bdb07df72995ed61c20782bfcdc8
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2021-01-10 12:52:16 +0530

    Linked list

---
+++ merge k sorted linked list.cpp
@@ -0,0 +1,47 @@
+Node* SortedMerge(Node* a, Node* b)
+{
+    Node* result = NULL;
+ 
+    /* Base cases */
+    if (a == NULL)
+        return (b);
+    else if (b == NULL)
+        return (a);
+ 
+    /* Pick either a or b, and recur */
+    if (a->data <= b->data) {
+        result = a;
+        result->next = SortedMerge(a->next, b);
+    }
+    else {
+        result = b;
+        result->next = SortedMerge(a, b->next);
+    }
+ 
+    return result;
+}
+
+Node* mergeKLists(Node* arr[], int last)
+{
+    // repeat until only one list is left
+    last = last-1;
+    while (last != 0) {
+        int i = 0, j = last;
+ 
+        // (i, j) forms a pair
+        while (i < j) {
+            // merge List i with List j and store
+            // merged list in List i
+            arr[i] = SortedMerge(arr[i], arr[j]);
+ 
+            // consider next pair
+            i++, j--;
+ 
+            // If all pairs are merged, update last
+            if (i >= j)
+                last = j;
+        }
+    }
+ 
+    return arr[0];
+}
