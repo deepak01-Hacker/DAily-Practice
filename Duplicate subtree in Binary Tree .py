50023ecb8b49d4e365e7c2b52e6cc39a6f19f3ec
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2021-01-17 18:01:52 +0530

    Binary Tree

---
+++ Duplicate subtree in Binary Tree .py
@@ -0,0 +1,125 @@
+#User function Template for python3
+'''
+# Node Class:
+class Node:
+    def _init_(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+        '''
+class Solution:
+    def inorder(self,root,v):
+        if root is None:
+            return
+        self.inorder(root.left,v)
+        v.append(root.data)
+        self.inorder(root.right,v)
+        
+    def dupSub(self, root):
+        v = []
+        self.inorder(root,v)
+        n = len(v)
+        lps = [0]*n
+        temp = 0
+        i = 1
+        len_ = 0
+        while(i < n):
+            if v[i] == v[len_]:
+                if len_ == i-1 or len_ == i-2:
+                    lps[i] = 0
+                    i += 1
+                    len_+=1
+                else:
+                    lps[i] = len_
+                    i += 1
+                    len_ += 1
+                    temp += 1
+            else:
+                if len_ !=0:
+                    len_ = lps[len_-1]
+                else:
+                    lps[i] = 0
+                    i += 1
+                temp = 0
+            if temp >= 2:
+                return 1
+        return 0
+            
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        ob = Solution()
+        print(ob.dupSub(root))
+# } Driver Code Ends
