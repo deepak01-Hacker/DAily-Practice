26efbd64c158bff5cfb4680a9c1b0f3743f1a6b2
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2021-02-11 15:04:44 +0530

    Stack

---
+++ Design a stack with operations on middle element.py
@@ -0,0 +1,12 @@
+How to implement a stack which will support following operations in O(1) time complexity?
+1) push() which adds an element to the top of stack.
+2) pop() which removes an element from top of stack.
+3) findMiddle() which will return middle element of the stack.
+4) deleteMiddle() which will delete the middle element.
+Push and pop are standard stack operations. 
+
+
+
+
+
+ANSWER : USE DOUBLY LINKEDLIST
