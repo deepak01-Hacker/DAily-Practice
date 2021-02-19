6b44155df790c598cbefc90b231fe246f18917a5
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2021-02-19 13:47:29 +0530

    Recursion

---
+++ Queue Reversal using Cpp.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+queue<int> rev(queue<int> q);
+int main()
+{
+    int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<int> q; 
+    int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    queue<int> a=rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}// } Driver Code Ends
+
+
+//function Template for C++
+
+void revUtil(queue<int> &q){
+    int x;
+    if (q.size() == 0){
+        return;
+    }
+    x = q.front();
+    q.pop();
+    revUtil(q);
+    q.push(x);
+}
+
+queue<int> rev(queue<int> q)
+{   
+    revUtil(q);
+    return q;
+    
+    
+}
